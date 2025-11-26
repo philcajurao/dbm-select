bfad18a6d2e32924c25c62ea9fa780afca8fa1a4
phil <64123755+philcajurao@users.noreply.github.com>
2025-11-27 07:26:19 +0800

    Polished UI Ready for client testing

--- Models/ImageItem.cs
+++ Models/ImageItem.cs
@@ -4,8 +4,8 @@ namespace dbm_select.Models
 {
     public class ImageItem
     {
-        public Bitmap Bitmap { get; set; }
-        public string FileName { get; set; }
-        public string FullPath { get; set; }
+        public required Bitmap Bitmap { get; set; }
+        public required string FileName { get; set; }
+        public required string FullPath { get; set; }
     }
 }
